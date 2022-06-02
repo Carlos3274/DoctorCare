c618feb144f635c21d8efc9d4c9e9d6623137e3a
José Carlos Araújo Pontes <jose.carlos.araujo07@aluno.ifce.edu.br>
2022-06-02 20:05:03 -0300

    Update main.js
    Correção erro
    const navigation = document.querySelector('nav')

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function activateMenuAtCurrentSection(section) {
 
 
 function showNavOnScroll() {
-
+    const navigation = document.querySelector('nav')
     if(scrollY > 0 ){
         navigation.classList.add('scroll')
     } else{
